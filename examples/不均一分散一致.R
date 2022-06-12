83e8d7d0e30f6a4b3e68830afabfbcc386f55554
Takahiro Konno <t.konno@shishimaro.co.jp>
2022-06-12 20:15:05 +0900

    add: HC推定量のサンプルコード

--- examples/不均一分散一致.R
+++ examples/不均一分散一致.R
@@ -2,3 +2,26 @@
 data("airquality")
 airquality
 
+# 通常の回帰
+fit <- glm(Ozone ~ ., data=airquality)
+summary(fit)
+
+# HC回帰
+library(AER)
+bvar <- vcovHC(fit)
+round(bvar, 1)
+
+sqrt(bvar['Wind', 'Wind'])
+
+# bootstrap
+B <- 10000
+beta <- vector(length = B)
+n <- nrow(airquality)
+
+for(b in 1:B){
+    bs = sample.int(n, n, replace = TRUE)
+    bsfit <- lm(Ozone ~ ., data=airquality, subset = bs)
+    beta[b] <- coef(bsfit)['Wind']
+}
+
+sd(beta)
