8054a634316836f01bb32f360854aec027d95068
Takahiro Konno <t.konno@shishimaro.co.jp>
2022-06-29 02:15:14 +0900

    add: ブートストラップ例を追加

--- practice/1_bootstrap.R
+++ practice/1_bootstrap.R
@@ -4,9 +4,9 @@ library(tidyverse)
 browser = read.csv("./examples/web-browsers.csv")
 browser %>% head()
 
-# ------------------------------------------------------------------------------
-# nonparametric bootstrap
-# ------------------------------------------------------------------------------
+# -----
+# 例1 : ブートストラップ標本平均
+# -----
 
 B <- 1000 # ブートストラップサンプル数
 mub <- c() # 再標本の平均の格納先
@@ -17,3 +17,24 @@ for (b in 1:B){
 }
 
 sd(mub) # 標本平均の分散
+
+
+# -----
+# 例2 : 線形回帰
+# -----
+
+# 通常の線形回帰
+linreg <- glm(log(spend) ~ broadband + anychildren, data = browser)
+summary(linreg)
+
+# ブートストラップ推定値
+B <- 1000
+betas <- c()
+
+for (b in 1:B) {
+    samp_b <- sample.int(nrow(browser), replace=TRUE) # 再標本
+    reg_b <- glm(log(spend) ~ broadband + anychildren, data = browser[samp_b, ]) # 再標本で回帰
+    betas <- rbind(betas, coef(reg_b)) # 係数をbetasに格納
+}
+
+betas[1:5, ] # はじめの5回の回帰の係数を表示する
